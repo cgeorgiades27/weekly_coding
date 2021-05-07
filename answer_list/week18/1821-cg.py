6b640347fd1c32354b5329f5baa1d38921238ca1
Chris Georgiades <cgeorgiades@adaptivehpc.com>
2021-05-07 15:58:09 -0400

    updating week 18

--- answer_list/week18/1821-cg.py
+++ answer_list/week18/1821-cg.py
@@ -10,15 +10,13 @@ class Solution:
             self.table[i[0]].append(i)
         for i in self.table:
             self.table[i].sort()
-        # print(self.table)
 
     def autocomplete(self, word):
         # Fill this in.
         arr = []
-        for i in len(word):
-            if self.table[word[0]][1] == word[1]
-        return self.table[word[0]]
-
+        for i in range(1, len(word)):
+            
+            
 
 s = Solution()
 s.build(['dog', 'dark', 'cat', 'door', 'dodge'])
---
+++ answer_list/week18/1821.-cg.py
@@ -0,0 +1,16 @@
+# Chris Georgiades
+# week 18, 2021
+
+
+class Solution:
+    def build(self):
+      # Fill this in.
+
+    def autocomplete(self, word):
+      # Fill this in.
+
+
+s = Solution()
+s.build(['dog', 'dark', 'cat', 'door', 'dodge'])
+print(s.autocomplete('do'))
+# ['dog', 'door', 'dodge']
