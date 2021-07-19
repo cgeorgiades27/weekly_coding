c353972724c0a59558ad2c839869b48b5789ae20
z3ke1r <chrisrobertson.gis@gmail.com>
2021-07-19 15:37:18 -0600

    Fixed some formatting

--- answer_list/week28/cr2821.py
+++ answer_list/week28/cr2821.py
@@ -4,11 +4,11 @@ def last_man_standing(souls: int, skipafew: int) -> int:
     """Takes a number of people 'souls' to be executed and an increment 'skipafew', returns the last to be executed"""
     cnt = 0
     goner = 0
-    soul_list = [x for x in range(1, souls + 1)]
+    soul_list = [x for x in range(1, souls+1)]
     while souls > 1:
         cnt += 1
         goner += 1
-        if cnt == skipafew - 1:
+        if cnt == skipafew-1:
             soul_list.pop(goner)
             souls -= 1
             cnt = 0
