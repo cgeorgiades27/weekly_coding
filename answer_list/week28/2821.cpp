9c701a7a88b1f52a774bb4eee34ac7b7160cdf44
Chris Georgiades <cgeorgiades@adaptivehpc.com>
2021-07-19 11:57:08 -0400

    completed week28 -cg

---
+++ answer_list/week28/2821.cpp
@@ -0,0 +1,64 @@
+/*
+######################
+# Week 28, 2021 - CG #
+######################
+
+There are `N` prisoners standing in a circle, waiting to be executed. The executions are carried out starting with the kth person, and removing every successive kth person going clockwise until there is no one left.
+
+Given N and k, write an algorithm to determine where a prisoner should stand in order to be the last survivor.
+
+For example, if `N = 5` and `k = 2`, the order of executions would be `[2, 4, 1, 5, 3]`, so you should return `3`.
+
+Bonus: Find an `O(log N)` solution if `k = 2`.
+*/
+
+#include <iostream>
+#include <bitset>
+#include <cstdlib>
+
+int main(int argc, char *argv[])
+{
+  if (argc < 3)
+    {
+      std::cerr << "Too few args. USAGE ./exec N K\n";
+      exit(1);
+    }
+  
+  size_t N = std::atoi(argv[1]);
+  size_t K = std::atoi(argv[2]);
+  std::cout << "N = " << N << ", K = " << K << std::endl;
+
+  typedef std::bitset<16> b_set;
+  
+  // prepare
+  b_set bset;
+  size_t count = 0;
+  for (size_t i = 0; i < N; ++i)
+    bset.set(i);
+  
+  while (bset.count() > 1)
+    {
+      for (size_t i = 0; i < N; ++i)
+	{
+	  if (bset.test(i))
+	   {
+	     ++count;
+	     if (count == K)
+	       {
+		 bset.reset(i);
+		 count = 0;
+	       }
+	   }
+	}
+    }
+
+  size_t last;
+  for (size_t i = 0; i < bset.size(); ++i)
+    {
+      if (bset.test(i))
+	last = i + 1; // adjust for index offset
+    }
+
+  std::cout << "Best place to start: " << last << std::endl;
+  return 0;
+}
