d3e723a5b88d1883eed6a78525bf3fc54080fbf5
cgeorgiades27 <cgeorgiades27@gmail.com>
2021-08-22 18:55:06 -0400

    completed week 33

--- answer_list/week33/3321.go
+++ answer_list/week33/3321.go
@@ -25,46 +25,54 @@ package main
 import "fmt"
 
 func main() {
+	// my_list := []int{3, 4, 6, 10, 11, 15, 16, 17}
+	// alices_list := []int{1, 5, 8}
 
-	// my_list := []int{3, 4, 6, 10, 11, 15}
-	// alices_list := []int{1, 5, 8, 12, 14, 19}
-
-	my_list := []int{3, 4, 6, 10, 11, 15, 16, 17}
-	alices_list := []int{1, 5, 8}
+	my_list := []int{3, 4, 6, 10, 11, 15}
+	alices_list := []int{1, 5, 8, 12, 14, 19}
 
 	fmt.Println(merge_lists(my_list, alices_list))
 }
 
+/**
+** Merge sort approach **
+**/
 func merge_lists(l1 []int, l2 []int) []int {
-
+	// temp pointer to hold appended value
 	var q *int
 	var final_list []int
 
+	// pointers to the front of each list
 	var p1 *int = &l1[0]
 	var p2 *int = &l2[0]
 
 	for (len(l1) > 0) && (len(l2) > 0) {
-
-		if len(l1) < 2 {
-			final_list = append(final_list, l2...)
-			return final_list
-		}
-		if len(l2) < 2 {
-			final_list = append(final_list, l1...)
-			return final_list
-		}
-
+		// check first list elements
 		if *p2 < *p1 {
-			q = &l2[0]
-			p2 = &l2[1]
-			l2 = l2[1:]
+			q = p2
+			if len(l2) > 1 {
+				p2 = &l2[1]
+				l2 = l2[1:]
+			} else {
+				l2 = l2[:0]
+			}
 			final_list = append(final_list, *q)
 		} else {
-			q = &l1[0]
-			p1 = &l1[1]
-			l1 = l1[1:]
+			q = p1
+			if len(l1) > 1 {
+				p1 = &l1[1]
+				l1 = l1[1:]
+			} else {
+				l1 = l1[:len(l1)-1]
+			}
 			final_list = append(final_list, *q)
 		}
+	}
+	// append what remains of the other sorted list
+	if len(l1) > 0 {
+		final_list = append(final_list, l1...)
+	} else {
+		final_list = append(final_list, l2...)
 	}
 	return final_list
 }
