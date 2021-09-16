006fbed6fde7de7d1e1a0bcdc2d67f443217842c
z3ke1r <chrisrobertson.gis@gmail.com>
2021-09-16 13:25:37 -0600

    Added week 37 solution

---
+++ answer_list/week37/3721-cr.py
@@ -0,0 +1,31 @@
+## Sieve of Eratosthenes O(log n log n)
+
+
+def get_primes(limit: int) -> list:
+    bool_dict = {x:True for x in range(2, limit)}
+    prime_set = []
+
+    if limit < 3:
+        return list()
+
+    for x in bool_dict.keys():
+        if bool_dict[x]:
+            prime_set.append(x)
+        total = x*x
+        while total < limit:
+            bool_dict[total] = False
+            total += x
+
+    return prime_set
+
+
+# Tests
+small = 100
+medium = 1000
+large = 100000
+dont_do_it = 100000000
+
+print(get_primes(small))
+print(get_primes(medium))
+#print(get_primes(large))
+
