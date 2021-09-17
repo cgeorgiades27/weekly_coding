7776b922f98ea6a4e9b600cd209f3c7d1d8510a1
Chris Georgiades <cgeorgiades@adaptivehpc.com>
2021-09-17 10:13:09 -0400

    completed week 37

--- answer_list/week37/3721-cg.go
+++ answer_list/week37/3721-cg.go
@@ -13,13 +13,24 @@ package main
 import (
 	"fmt"
 	"math"
+	"time"
 )
 
 func main() {
-	Sieve(100)
+	start := time.Now()
+	var n int
+	var inf bool
+	fmt.Printf("Enter the max prime (>2) or -1 for infinite: ")
+	fmt.Scanln(&n)
+	if n == -1 {
+		n = 100
+		inf = true
+	}
+	Sieve(n, inf)
+	fmt.Printf("total time for %d: %f\n", n, time.Now().Sub(start).Seconds())
 }
 
-func Sieve(n int) {
+func Sieve(n int, inf bool) {
 	bitslc := make([]bool, n)
 	max := n
 	sqrtmax := math.Ceil(math.Sqrt(float64(max)))
@@ -38,11 +49,42 @@ func Sieve(n int) {
 			}
 		}
 	}
-	primeslc := []int{}
 	for i := range bitslc {
 		if bitslc[i] {
-			primeslc = append(primeslc, i)
+			fmt.Println(i)
 		}
 	}
-	fmt.Println(primeslc)
+
+	if inf {
+		for {
+			// record old and reset max
+			oldLen := len(bitslc)
+			max = len(bitslc) * 2
+			sqrtmax = math.Ceil(math.Sqrt(float64(max)))
+
+			// resize the bit array
+			new := make([]bool, (len(bitslc)+1)*2)
+			copy(new, bitslc)
+			bitslc = new
+
+			for i := oldLen - 1; i < max; i++ {
+				bitslc[i] = true
+			}
+			for i := oldLen; i < max; i += 2 {
+				bitslc[i] = false
+			}
+			for i := oldLen; i < int(sqrtmax); i += 2 {
+				if bitslc[i] {
+					for j := i * i; j < max; j += 1 {
+						bitslc[j] = false
+					}
+				}
+			}
+			for i := oldLen; i < max; i++ {
+				if bitslc[i] {
+					fmt.Println(i)
+				}
+			}
+		}
+	}
 }
