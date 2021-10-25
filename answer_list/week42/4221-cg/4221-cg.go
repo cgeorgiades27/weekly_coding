582124f636f1e17c1e9cbcc05dec88dc2f8be74c
Chris Georgiades <cgeorgides27@gmail.com>
2021-10-25 12:22:10 +0000

    progress on week42

---
+++ answer_list/week42/4221-cg/4221-cg.go
@@ -0,0 +1,27 @@
+// Given a 32-bit positive integer N,
+// determine whether it is a power of four in faster than O(log N) time.
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var uint32Arr, randUint []uint32
+	for i := 0; i < 32/2; i++ {
+		uint32Arr = append(uint32Arr, 1<<(2*i))
+	}
+	for i := 0; i < 16; i++ {
+		randUint = append(randUint, rand.Uint32())
+	}
+	// for _, elem := range uint32Arr {
+	// fmt.Printf("%12d:%7t\n", elem, isPowerOfFour(elem))
+	// }
+	// for _, elem := range randUint {
+	// 	fmt.Printf("%12d:%7t\n", elem, isPowerOfFour(elem))
+	// }
+}
+
+func isPowerOfFour(num uint32) bool {
+	// if
+}
