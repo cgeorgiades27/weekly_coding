839a381bd3e73280a6519e3e909c776f194aa87e
z3ke1r <chrisrobertson.gis@gmail.com>
2021-12-07 15:37:35 -0700

    Added week 48 answer

--- answer_list/week48/4821-cr.go
+++ answer_list/week48/4821-cr.go
@@ -6,32 +6,37 @@ import (
 	"strings"
 )
 
+// Compares value at x[i] with previous value
+// If they dont match writes count and value to encode
 func encode(decoded_s string) string {
 	var encoded strings.Builder
-	l := len(decoded_s)
+	length := len(decoded_s)
 	i := 1
 	total := 1
 
-	for i < l {
+	for ; i < length; i++ {
 		if decoded_s[i] != decoded_s[i-1] {
-			encoded.WriteString(strconv.Itoa(total) + string(decoded_s[i-1]))
+			encoded.WriteString(strconv.Itoa(total))
+			encoded.WriteString(string(decoded_s[i-1]))
 			total = 0
 		}
-		i++
 		total++
 
 	}
-	encoded.WriteString(strconv.Itoa(total) + string(decoded_s[i-1]))
+	encoded.WriteString(strconv.Itoa(total))
+	encoded.WriteString(string(decoded_s[i-1]))
 
 	return encoded.String()
 }
 
+// Gets int (x) from each even index until end
+// writes value[i+1] to the decode x number of times
 func decode(encoded_s string) string {
 	var decoded strings.Builder
 	i := 0
 	length := len(encoded_s)
 	for ; i < length; i += 2 {
-		multiple := int(encoded_s[i] - '0')
+		multiple := int(encoded_s[i] - '0') //int from ascii byte
 		for t := 0; t < multiple; t++ {
 			decoded.WriteString(string(encoded_s[i+1]))
 		}
@@ -43,9 +48,9 @@ func decode(encoded_s string) string {
 }
 
 func main() {
-	s := "AAAABBBCCDAA"
-	e := encode(s)
-	d := decode(e)
-	fmt.Printf("Encoded: %s, Decoded: %s", e, d)
+	test_string := "AAAABBBCCDAA"
+	encoded_string := encode(test_string)
+	decoded_string := decode(encoded_string)
+	fmt.Printf("Encoded: %s, Decoded: %s", encoded_string, decoded_string)
 
 }
