21b1982c4a887eb6238306cef9329005c8ff3f17
Chris Georgiades <cgeorgides27@gmail.com>
2021-12-08 04:38:40 +0000

    completed week 48

---
+++ answer_list/week48/4821-cg/4821-cg_test.go
@@ -0,0 +1,53 @@
+// Week 48, 2021 - (Easy)
+//
+// This problem was asked by **Amazon**.
+// Run-length encoding is a fast and simple method of encoding strings. The basic idea is to represent repeated successive
+// characters as a single count and character. For example, the string ```"AAAABBBCCDAA"``` would be encoded as ```4A3B2C1D2A```.
+// Implement run-length encoding and decoding. You can assume the string to be encoded have no digits and consists solely
+// of alphabetic characters. You can assume the string to be decoded is valid.
+//
+// Run the test(s): go test -v
+// Run the benchmark: go test -bench=.
+package main
+
+import (
+	"strconv"
+	"testing"
+)
+
+const (
+	TestCase       = "AAAABBBCCDAA"
+	ExpectedResult = "4A3B2C1D2A"
+)
+
+func Encode(str string) string {
+	var bs string
+	for len(str) > 0 {
+		count := 0
+		ctx := str[0]
+		for len(str) > 0 && str[0] == ctx {
+			count++
+			if len(str) > 1 {
+				str = str[1:]
+			} else {
+				str = ""
+			}
+		}
+		bs = bs + strconv.Itoa(count)
+		bs = bs + string(ctx)
+	}
+	return bs
+}
+
+func TestRunEncoding(t *testing.T) {
+	res := Encode(TestCase)
+	if res != ExpectedResult {
+		t.Errorf("encode(%s) = %s; should be %s", TestCase, res, ExpectedResult)
+	}
+}
+
+func BenchmarkEncoding(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Encode(TestCase)
+	}
+}
