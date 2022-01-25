4c542c4d636715142a6c87c87633b531e653ba48
z3ke1r <chrisrobertson.gis@gmail.com>
2022-01-25 15:41:06 -0700

    Added solution 0322-cr

--- answer_list/2022/0322/cr/0322-cr.go
+++ answer_list/2022/0322/cr/0322-cr.go
@@ -9,6 +9,29 @@ type CacheLRU struct {
 	lru   string
 }
 
+func main() {
+	n := 5
+	lruCache := new(CacheLRU)
+	lruCache.cap = n
+	input := [25]string{"playstation.us", "botnet.ru", "botnet.ru",
+		"playstation.us", "playstation.us", "stonks.com",
+		"playstation.us", "amscotcashadvance.com", "peloton.com",
+		"cannedham.edu", "stonks.com", "amscotcashadvance.com",
+		"playstation.us", "of.com", "of.com", "google.com",
+		"cannedham.edu", "peloton.com", "amscotcashadvance.com", "stonks.com",
+		"google.com", "of.com", "amscotcashadvance.com", "go.dev", "google.com"}
+
+	for _, v := range input {
+		lruCache.add(v)
+		fmt.Printf("Added %q to the cache \n", v)
+	}
+	for _, v := range input {
+		x, y := lruCache.get(v)
+		fmt.Println(x, y)
+	}
+
+}
+
 func (c *CacheLRU) add(item string) {
 	if len(c.cache) == 0 {
 		c.cache = make(map[string]int)
@@ -31,27 +54,4 @@ func (c *CacheLRU) add(item string) {
 func (c *CacheLRU) get(key string) (int, bool) {
 	elem, ok := c.cache[key]
 	return elem, ok
-}
-
-func main() {
-	n := 5
-	lruCache := new(CacheLRU)
-	lruCache.cap = n
-	input := [25]string{"playstation.us", "botnet.ru", "botnet.ru",
-		"playstation.us", "playstation.us", "stonks.com",
-		"playstation.us", "amscotcashadvance.com", "peloton.com",
-		"cannedham.edu", "stonks.com", "amscotcashadvance.com",
-		"playstation.us", "of.com", "of.com", "google.com",
-		"cannedham.edu", "peloton.com", "amscotcashadvance.com", "stonks.com",
-		"google.com", "of.com", "amscotcashadvance.com", "go.dev", "google.com"}
-
-	for _, v := range input {
-		lruCache.add(v)
-		fmt.Printf("Added %q to the cache \n", v)
-	}
-	for _, v := range input {
-		x, y := lruCache.get(v)
-		fmt.Println(x, y)
-	}
-
 }
