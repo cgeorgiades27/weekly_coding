9fd78ad94679c85cd688161e29e752f30b5bfa5b
darkswordss <bpym77@gmail.com>
2022-02-07 12:30:47 -0500

    working

--- answer_list/2022/0522/JG/main.cpp
+++ answer_list/2022/0522/JG/main.cpp
@@ -11,36 +11,39 @@
 return the longest palindrom in a given stting
 
 
-/**
- * Function that returns the best position to stand to be the last person standing.
+/*
+ * Function that returns the longest palindrome substring in a given string.
  *
  * 
- * @param k     first to die i.e place to start 
- * @param n     amount of prisoners
+ * @param parseMe     string to search for lingest palindrome in
  
  */
 
 std::string findLongestPalindrome(std::string parseMe)
 {
-  int counter = 0;
-  std::string returnMe = "";
-  for (auto currentChar : parseMe)
+  int counter = 0; // used to keep track ofwhat position from the right we are at
+  std::string currentLongestPalindrome = "";
+  std::string absoulteGlobalPalindrome = "";
+  for (unsigned int index = 0; index < parseMe.length() - 1; index++)
   {
-    std::cout << currentChar << std::endl;
-    counter = 1; //start checking from the last char 
-    if (currentChar != parseMe.at(parseMe.length() - counter))
-      returnMe = "";
-
+    currentLongestPalindrome = "";
+    counter = 0; //start checking from the last char
+    unsigned int tmpindex = index;
     do
     {
       counter++;
-      if (currentChar == parseMe.at(parseMe.length() - counter))
-        returnMe += currentChar;
-      break;
-    } while (parseMe.length() - counter != 1);
+      if (parseMe.at(tmpindex) == parseMe.at(parseMe.length() - counter))
+      {
+        currentLongestPalindrome += parseMe.at(tmpindex);
+        tmpindex++;
+      }
+    } while (parseMe.length() - counter > 0 && tmpindex < parseMe.length());
+
+    if (currentLongestPalindrome.length() > absoulteGlobalPalindrome.length()) // if the current palindrome is longer than the absoulte palindrome make it the absolute palindrome
+      absoulteGlobalPalindrome = currentLongestPalindrome;
   }
-  std::cout << returnMe;
-  return returnMe;
+  std::cout << " Longest paindrome in, " << parseMe << " is " << absoulteGlobalPalindrome << std::endl;
+  return absoulteGlobalPalindrome;
 }
 
 int main()
@@ -49,8 +52,17 @@ int main()
   //# Test Program
 
   std::string test = "adabbae";
+  assert((findLongestPalindrome(test) == "abba") && "abba should be longest palindrome");
 
-  assert((findLongestPalindrome(test) == "abba") && "aaa should be longest palindrome");
+  std::string test2 = "adababae";
+
+  assert((findLongestPalindrome(test2) == "ababa") && "ababa should be longest palindrome");
+
+  std::string test3 = "cbbd";
+
+  assert((findLongestPalindrome(test3) == "bb") && "bb should be longest palindrome");
+
+  findLongestPalindrome("lollipop");
 
   return 0;
 }
