9a8b1ce0e5cdef782fe2f3e1a509a1c79af43bf4
z3ke1r <chrisrobertson.gis@gmail.com>
2022-04-04 14:17:31 -0600

    added 1222 solution

---
+++ answer_list/2022/1222/cr/1222-cr.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
+
+func main() {
+	input := []string{"01:45", "06:27", "10:18", "11:11", "07:30",
+		"09:00", "09:59", "12:00", "12:11", "02:19",
+		"04:20", "08:04", "01:08", "12:30"}
+
+	timesToAngles(input)
+
+}
+
+func timesToAngles(times []string) {
+	for _, v := range times {
+		hour := getHour(v)
+		minute := getMinute(v)
+		angle := getAngle(hour, minute)
+
+		fmt.Printf("%v has a angle of %v°\n", v, angle)
+	}
+}
+
+func getHour(strTime string) float64 {
+	strHr := strTime[0:2]
+	hour, _ := strconv.ParseFloat(strHr, 64)
+
+	return hour
+}
+
+func getMinute(strTime string) float64 {
+	strMn := strTime[3:]
+	minute, _ := strconv.ParseFloat(strMn, 64)
+
+	return minute / 5
+}
+
+func getAngle(h, m float64) float64 {
+	diff := h - m
+	angle := math.Round(math.Abs((diff * 30)))
+
+	if angle > 180 {
+		angle = 360 - angle
+	}
+
+	return angle
+}
