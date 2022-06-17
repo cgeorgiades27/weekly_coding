8ef7caf50e92da9bb8e34255a5d8483bb94fcce8
Chris Georgiades <cg@cs.dev>
2022-06-17 09:04:16 -0400

    added solution with division

--- answer_list/2022/2422/cg/2422.cpp
+++ answer_list/2022/2422/cg/2422.cpp
@@ -1,38 +1,36 @@
 
 /*****************
  * WEEK 24, 2022 *
- *****************/ 
+ *****************/
 
 #include <iostream>
 #include <vector>
 
 typedef std::vector<int> vec;
 
-vec a1{1,2,3,4,5};
-vec a2{3,2,1};
+vec a1{1, 2, 3, 4, 5};
+vec a2{3, 2, 1};
 
-vec* getArrSum(vec&);
+vec getArrSum(vec &);
 
 int main()
 {
-  vec* response = getArrSum(a1);
-  for (auto i: *response)
+  vec response = getArrSum(a1);
+  for (auto i : response)
     std::cout << i << std::endl;
-  
+
   return 0;
 }
 
-vec* getArrSum(vec &v)
+vec getArrSum(vec &v)
 {
-
   int totalSum = 1;
   for (auto i : v)
-      totalSum *= i;
+    totalSum *= i;
 
-  vec* res = new vec;
+  vec res;
   for (auto i : v)
-    (*res).push_back(i/totalSum);
+    res.push_back(totalSum / i);
 
   return res;
 }
-
